e27213ab350aa6e8c8e64d911aecfc7d46df75ff
shreya1881 <2704shreya@gmail.com>
2022-10-07 20:59:20 +0530

    .

---
+++ reverse-words-in-a-string-III.cpp
@@ -0,0 +1,11 @@
+ int i=0;
+        for(int j=0;j<s.length();j++)
+        {
+            if(s[j]==' ')
+            {
+                reverse(s.begin()+i,s.begin()+j);
+                i=j+1;
+            }
+        }
+        reverse(s.begin()+i,s.end());
+        return s;
