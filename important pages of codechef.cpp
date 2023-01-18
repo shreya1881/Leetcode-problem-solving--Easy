92d0bf030299bd68c6378f9cb94a13bd8ce49a43
shreya1881 <2704shreya@gmail.com>
2023-01-18 20:40:33 +0530

    .

---
+++ important pages of codechef.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	if(a==1 && b==0) cout<<"https://www.codechef.com/contests";
+	else if(a==0) cout<<"https://www.codechef.com/practice";
+	else if(b==1 && a==1) cout<<"https://discuss.codechef.com";
+	return 0;
+}
