cd5a26023e87885e647f064d96b35251fa7748e5
shreya1881 <2704shreya@gmail.com>
2023-01-19 18:31:56 +0530

    .

---
+++ cyclic quadrilateral.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b,c,d;
+	    cin>>a>>b>>c>>d;
+	    if( ((a+c) == 180 ) && ((b+d)==180)) cout<<"YES\n";
+	    else cout<<"NO\n";
+	}
+	return 0;
+}
