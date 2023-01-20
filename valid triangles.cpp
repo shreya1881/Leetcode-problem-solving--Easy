8e013a1322ce1de6177253963d79d04792c2a261
shreya1881 <2704shreya@gmail.com>
2023-01-20 21:13:48 +0530

    .

---
+++ valid triangles.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if(a+b+c == 180) cout<<"YES\n";
+	    else cout<<"NO\n";
+	    
+	}
+	return 0;
+}
